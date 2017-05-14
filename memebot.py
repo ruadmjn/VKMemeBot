d0bf5fca3fd0ed9e71e3fd038ae5b6f08f893916
ruadmjn <ruadmjn@gmail.com>
2017-05-14 04:53:51 +0300

    great optimization

--- memebot.py
+++ memebot.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import time
 from config import config
 import telebot
 from lib.core import vkMemes
@@ -6,16 +7,19 @@ bot = telebot.TeleBot(config.tg_token)
 
 @bot.message_handler(commands=["meme", "start", "donat"])
 def SendMeme(message):
-    if message.text == "/start":
-        bot.reply_to(message, "Use /donat to support my future")
-        bot.reply_to(message, "Use /meme to orat do gor")
-    if message.text == "/donat":
-        bot.reply_to(message, "Here - 4817 7600 1285 8563")
-    if message.text == "/meme":
-        try:
+    try:
+        if message.text == "/start":
+            bot.reply_to(message, "Use /donat to support my future")
+            bot.reply_to(message, "Use /meme to orat do gor")
+        if message.text == "/donat":
+            bot.reply_to(message, "Here - 4817 7600 1285 8563")
+        if message.text == "/meme":
+            time.sleep(1)
             bot.send_photo(message.chat.id, vkMemes.GetMeme())
-        except:
-            bot.reply_to(message, "Too many memes. Repeat /meme ")
+    except:
+        bot.reply_to(message, "Something went wrong. Repeat /meme ")
+        time.sleep(2)
+        SendMeme(message)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
