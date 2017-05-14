4e850310ebe7552541ba20d8851e0d388625e37f
ruadmjn <ruadmjn@gmail.com>
2017-05-15 01:20:40 +0300

    better randomize

--- lib/core.py
+++ lib/core.py
@@ -11,13 +11,13 @@ class vkMemes():
         session = vk.Session(config.vk_secret)
         vkapi = vk.API(session=session)
         try:
-            if len(self.picList) < 100:
-                for memePage in range(0, 2):
+            if len(self.picList) < 250:
+                for memePage in range(0, 3):
                     offset = memePage * 100
                     posts = vkapi.wall.get(owner_id="-45745333", domain="vk.com/4ch", filter="owner", version="5.64", count=100, offset=offset)
                     for post in posts:
                         if type(post) == type({}):
-                            if post["attachment"]["type"] == "photo" and post["likes"]["count"] > 3000:
+                            if post["attachment"]["type"] == "photo" and post["likes"]["count"] > 4000:
                                 if post["attachment"]["photo"]["src_big"] not in self.picList:
                                     self.picList.append(post["attachment"]["photo"]["src_big"])
             return self.picList[random.randint(0, len(self.picList)-1)]
@@ -28,5 +28,5 @@ class vkMemes():
             else:
                 return self.picList[random.randint(0, len(self.picList)-1)]
         finally:
-            if len(self.picList) > 200:
+            if len(self.picList) >= 250:
                 self.picList = []
