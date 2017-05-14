df72fa52b5140e0eb6a5a8fda2f9030041052a61
ruadmjn <ruadmjn@gmail.com>
2017-05-14 15:55:21 +0300

    some api optimization
    few api calls

--- lib/core.py
+++ lib/core.py
@@ -11,13 +11,15 @@ class vkMemes():
         session = vk.Session(config.vk_secret)
         vkapi = vk.API(session=session)
         try:
-            for memePage in range(0, 2):
-                offset = memePage * 100
-                posts = vkapi.wall.get(owner_id="-45745333", domain="vk.com/4ch", filter="owner", version="5.64", count=100, offset=offset)
-                for post in posts:
-                    if type(post) == type({}):
-                        if post["attachment"]["type"] == "photo" and post["likes"]["count"] > 2000:
-                            self.picList.append(post["attachment"]["photo"]["src_big"])
+            if len(self.picList) < 100:
+                for memePage in range(0, 2):
+                    offset = memePage * 100
+                    posts = vkapi.wall.get(owner_id="-45745333", domain="vk.com/4ch", filter="owner", version="5.64", count=100, offset=offset)
+                    for post in posts:
+                        if type(post) == type({}):
+                            if post["attachment"]["type"] == "photo" and post["likes"]["count"] > 3000:
+                                if post["attachment"]["photo"]["src_big"] not in self.picList:
+                                    self.picList.append(post["attachment"]["photo"]["src_big"])
             return self.picList[random.randint(0, len(self.picList)-1)]
         except Exception as ex:
             if len(self.picList) == 0:
@@ -26,5 +28,5 @@ class vkMemes():
             else:
                 return self.picList[random.randint(0, len(self.picList)-1)]
         finally:
-            if len(self.picList) > 3000:
+            if len(self.picList) > 200:
                 self.picList = []
