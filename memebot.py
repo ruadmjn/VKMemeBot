2891c81a3dc9417f43bc45313d41f712867ecc97
ruadmjn <ruadmjn@gmail.com>
2017-05-14 03:02:27 +0300

    more info

--- memebot.py
+++ memebot.py
@@ -4,11 +4,15 @@ import telebot
 from lib.core import vkMemes
 bot = telebot.TeleBot(config.tg_token)
 
-@bot.message_handler(commands=["meme", "start"])
+@bot.message_handler(commands=["meme", "start", "donat"])
 def SendMeme(message):
     if message.text == "/start":
-        bot.send_message(message.chat.id, "Use /meme to orat do gor")
-    bot.send_photo(message.chat.id, vkMemes.GetMeme())
+        bot.reply_to(message, "Use /donat to support my future")
+        bot.reply_to(message, "Use /meme to orat do gor")
+    if message.text == "/donat":
+        bot.reply_to(message, "Here - 4817 7600 1285 8563")
+    if message.text == "/meme":
+        bot.send_photo(message.chat.id, vkMemes.GetMeme())
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
