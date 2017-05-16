e10bb85d2149b371f10112c7505468dcc12cc8f4
ruadmjn <ruadmjn@gmail.com>
2017-05-16 23:35:55 +0300

    docs & list groups edited

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-VKMemeBot
+# How to run as service
+
+- nano /lib/systemd/system/memebot.service
+
+[Unit]
+
+Description=My Script Service
+
+After=multi-user.target
+
+
+[Service]
+
+Type=idle
+
+ExecStart=/usr/bin/python3.4 /root/VKMemeBot/memebot.py
+
+
+[Install]
+
+WantedBy=multi-user.target
+
+- chmod 644 /lib/systemd/system/memebot.service
+
+- systemctl daemon-reload
+
+- systemctl enable memebot.service
+
+- service memebot.service start
--- lib/groups.py
+++ lib/groups.py
@@ -4,15 +4,15 @@ class groups:
     groups = {
         "amoral": [
             {"owner_id": "-45745333", "domain": "vk.com/4ch", "likes": "6000", "type": "photo"},
-            {"owner_id": "-89157850", "domain": "vk.com/memology", "likes": "4000", "type": "photo"},
-            {"owner_id": "-60767284", "domain": "vk.com/frantic_club", "likes": "4000", "type": "photo"},
+            {"owner_id": "-135209264", "domain": "vk.com/bot_maxim", "likes": "15000", "type": "photo"},
+            {"owner_id": "-60767284", "domain": "vk.com/frantic_club", "likes": "100", "type": "photo"},
         ],
         "story": [
-            {"owner_id": "-34215577", "domain": "vk.com/overhear", "likes": "1000", "type": "text"},
+            {"owner_id": "-34215577", "domain": "vk.com/overhear", "likes": "100", "type": "text"},
             {"owner_id": "-31480508", "domain": "vk.com/pikabu", "likes": "4000", "type": "post"},
         ],
         "gif": [
-            {"owner_id": "-39488246", "domain": "vk.com/gif_fak", "likes": "200", "type": "gif"},
-            {"owner_id": "-65047210", "domain": "vk.com/fun.gifs", "likes": "200", "type": "gif"},
+            {"owner_id": "-39488246", "domain": "vk.com/gif_fak", "likes": "50", "type": "gif"},
+            {"owner_id": "-65047210", "domain": "vk.com/fun.gifs", "likes": "50", "type": "gif"},
         ]
     }
